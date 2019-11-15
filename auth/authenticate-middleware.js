be83c2df294796243e4f84eb064aea08821b650b
Connor R. Sullivan <crsullivan@protonmail.com>
2019-11-15 10:58:20 -0700

    MVP

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,21 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require ('jsonwebtoken'); 
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    const secret = process.env.JWT_SECRET || 'Sprintville';
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'invalid creds'})
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
 };
